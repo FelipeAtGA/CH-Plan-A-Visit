fffa549237851c7b70cd47dce832be963eeff1d7
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-11 12:28:08 -0400

    add routes folder and visitorPlanner file

---
+++ routes/visitorPlanner.js
@@ -0,0 +1,16 @@
+// let the file know we are using express
+const express = require('express');
+
+// let the file know we are using a controller
+const controller = require('../controllers/dbController');
+
+// let the file know that we are using a views controller
+//const views = require('../controllers/viewController');
+
+// use an instance of router for the following calls
+const visitPlanRouter = express.Router();
+
+
+visitPlanRouter.route('/').post(controller.create);
+
+module.exports = visitPlanRouter;
