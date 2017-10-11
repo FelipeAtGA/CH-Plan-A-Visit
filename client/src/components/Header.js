69aa5eaf2d67d462270badc9d663c0a84212ed1a
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-11 00:16:33 -0400

    add links to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <nav>
+            <ul className='nav-menu'>
+              <li><Link to='/'><h2>Home</h2></Link></li>
+              <li>|</li>
+              <li><Link to='/current'><h2>Current Exhibitions</h2></Link></li>
+              <li>|</li>
+              <li><Link to='/search'><h2>Search the Collection</h2></Link></li>
+            </ul>
+          </nav>
         </header>
       </div>
     );
