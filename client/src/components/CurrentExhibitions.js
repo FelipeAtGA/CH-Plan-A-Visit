973fba0a99d02daaf0cc094abcef5a176fc16040
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-11 07:38:35 -0400

    create new branch and component CurrentExhibitions, edit file adding class

---
+++ client/src/components/CurrentExhibitions.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CurrentExhibitons extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Current Exhibitions</h1>
+      </div>
+
+    )
+  }
+}
