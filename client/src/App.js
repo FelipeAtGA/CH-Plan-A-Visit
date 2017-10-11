fd508b557d3d3fd2a37cd33fc1f1b71fa59b14d6
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-10 23:54:22 -0400

    add state, method, and routes to handle search form request

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,20 @@ import axios from 'axios';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import SearchForm from './components/SearchForm';
+
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       exhibits: [],
+      objects: [];
+      inputSearchValue: '',
+
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputSearchOnCahnge = this.handleInputSearchOnCahnge.bind(this);
   }
 
   componentDidMount() {
@@ -26,13 +33,50 @@ class App extends Component {
       console.log(err)
     });
   }
+
+  handleInputSearchOnCahnge(event) {
+    this.setState({
+      inputSearchValue: event.target.value,
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let keywords = encodeURI(this.state.inputSearchValue);
+    let url = 'https://api.collection.cooperhewitt.org/rest/'
+        url += '?method=cooperhewitt.search.collection&access_token='
+        url += '49525b3629ac5bb3d446da768b0305a7&has_images=1&page=1&per_page=10'
+        url += '&query='
+        url +=  keywords
+        url += '&description='
+        url +=  keywords;
+        //console.log('url', url);
+    axios(url)
+    .then((res) => {
+      this.setState((prevState) => {
+        return {
+          objects: res.data.objects,
+        }
+      })
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route exact path='/search'
+            render={(props) => <SearchForm
+              handleSubmit={this.handleSubmit}
+              inputSearchValue={ this.state.inputSearchValue }
+              handleInputSearchOnCahnge={ this.handleInputSearchOnCahnge }
+              objects={ this.state.objects }
+            />}
+          />
+        </Switch>
         <Footer />
       </div>
     );
