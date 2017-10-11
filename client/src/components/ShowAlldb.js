41d7e3a815e5d32ad36c79736566897074df9b7d
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-11 19:15:18 -0400

    create and edit ShowAlldb Component file

---
+++ client/src/components/ShowAlldb.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ShowAlldb extends Component {
+  render() {
+    return(
+      <section>
+        <h1>List of your items</h1>
+      </section>
+
+    )
+  }
+}
