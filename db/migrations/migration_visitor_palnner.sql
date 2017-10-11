e27a387aff133175c9a021708502ddff4aa0b81d
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-10 23:21:52 -0400

    create db, migrations, and seeds directory. create and edit migration, seed, and config files

---
+++ db/migrations/migration_visitor_palnner.sql
@@ -0,0 +1,10 @@
+\c visitor_palnner
+
+DROP TABLE IF EXISTS items;
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  img_url TEXT,
+  exhibit_url TEXT
+);
---
+++ db/seeds/item_seed_visitor_planner.sql
@@ -0,0 +1,8 @@
+\c visitor_palnner
+
+INSERT INTO items (title, img_url, exhibit_url)
+VALUES
+  ('Poster, London Music Festival, 1939',
+  'https://images.collection.cooperhewitt.org/254119_de83d10266e169dd_b.jpg',
+  'https://collection.cooperhewitt.org/objects/18446845/'
+  );
