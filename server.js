2adddce6b0cc0a16ffc65f26a9385dfc0b719b4d
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-10 22:39:51 -0400

    set up express server in server.js

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-server.js
+const express =  require('express'); // bring express library
+const path    = require('path'); // enable file and directory paths
+const app     = express(); // create an instance of express
+
+// bring all dependencies
+const logger         = require('morgan');
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
+const cors           = require('cors');
+
+// assign a port for server to run locally
+// and enable a port for future deployment
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(methodOverride('_method'));
+
+//set up logger and body-parser
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, }));
+
+
+//================= SETTING ROUTES ==================== //
+
+
+// set app to the root
+app.get('/', function (req, res) {
+  res.send('Hello from /api')
+});
+
+// handle 404 error
+app.get('*', function(req, res) {
+  res.status(404).send({message: 'Not found!'});
+});
+
+// making front and back end connection
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}, in ${app.get('env')} mode.`)
+});
