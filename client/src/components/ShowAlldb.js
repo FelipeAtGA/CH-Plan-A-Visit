484019a50f9044c161b303af5a2648bed4b5d78a
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-11 19:33:55 -0400

    add export default to ShowAlldb Component

--- client/src/components/ShowAlldb.js
+++ client/src/components/ShowAlldb.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class ShowAlldb extends Component {
   render() {
+    console.log('ShowAlldb data ', this.props.dbItems)
     return(
       <section>
         <h1>List of your items</h1>
@@ -10,3 +11,5 @@ class ShowAlldb extends Component {
     )
   }
 }
+
+export default ShowAlldb;
