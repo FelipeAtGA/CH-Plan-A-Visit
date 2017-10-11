eedf5e6618a897bd17abbe65b5b6eb8f2d77f648
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-10 23:37:52 -0400

    add constructor/state, componentDidMount, import axios, and set up api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      exhibits: [],
+    }
+  }
+
+  componentDidMount() {
+    axios('https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getList&access_token=49525b3629ac5bb3d446da768b0305a7&page=1&per_page=10')
+    .then((res) => {
+      console.log('axios', res);
+      this.setState((prevState) => {
+        return{
+          exhibits: res.data.exhibitions,
+        }
+      })
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
   render() {
     return (
       <div className="App">
