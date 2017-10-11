52e294a5311ab03af318dd37ef0f948fdff6efee
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-11 08:05:03 -0400

    add layout and props to CurrentExhibitions component

--- client/src/components/CurrentExhibitions.js
+++ client/src/components/CurrentExhibitions.js
@@ -5,6 +5,24 @@ class CurrentExhibitons extends Component {
     return(
       <div>
         <h1>Current Exhibitions</h1>
+        {this.props.exhibits.map((exhibit) => {
+          if( exhibit.is_active === '1'){
+            return(
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <h3>{ exhibit.title }</h3>
+                    </td>
+                    <td>
+                      <a href={ exhibit.url } target='_black'>view details</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            )
+          }
+        })}
       </div>
 
     )
