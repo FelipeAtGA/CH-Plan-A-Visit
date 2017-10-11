62e134e6bfb20aa1cbc6a34eda8c412c03287c6c
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-10 23:09:48 -0400

    create components directory, and Header and Footer files

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return(
+      <div>
+        <div className="App-footer">
+          <h6>Made with Love by Felipe Hernandez 2017 </h6>
+        </div>
+      </div>
+    )
+  };
+}
+
+export default Footer;
