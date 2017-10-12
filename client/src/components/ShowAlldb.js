a7342523400886c0c43c1b567a1a3a9df5b17258
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-12 15:15:47 -0400

    delete constructor and componentDidMount from ShowAlldb Component

--- client/src/components/ShowAlldb.js
+++ client/src/components/ShowAlldb.js
@@ -2,34 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class ShowAlldb extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      dbItems: [],
-
-    }
-  }
-
-  componentDidMount() {
-    axios('http://localhost:3001/api/planner')
-    .then((res) => {
-      this.setState((prevState) => {
-        console.log('didMount all ', res.data.data.items)
-        return{
-          dbItems: res.data.data.items,
-        }
-      })
-    })
-  }
-
   render() {
-    console.log('ShowAlldb data ', this.state.dbItems)
+    console.log('ShowAlldb data ', this.props.dbItems)
     return(
       <section>
         <h1>List of your items</h1>
         <table>
           <tbody>
-          {this.state.dbItems.map((dbItem) => {
+          {this.props.dbItems.map((dbItem) => {
               return(
                 <tr>
                   <td className='imgSearchedWraper'>
@@ -37,6 +17,13 @@ class ShowAlldb extends Component {
                   </td>
                   <td className='showAllTitles'>
                     <h3><a href={dbItem.exhibit_url} target='_blank'>{ dbItem.title }</a></h3>
+                  </td>
+                  <td>
+                    <button
+                      onClick={ (id) => {
+                        this.props.handleDeleteItem(dbItem.id);
+                      }}
+                    >Delete</button>
                   </td>
                 </tr>
               )}
