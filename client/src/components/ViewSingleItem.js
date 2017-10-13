35fb0e39b5a7afba4ed0be9ed332977d5fae9ba6
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-13 14:15:16 -0400

    ViewSingleItem edit file

--- client/src/components/ViewSingleItem.js
+++ client/src/components/ViewSingleItem.js
@@ -2,26 +2,27 @@ import React, { Component } from 'react';
 
 class ViewSingleItem extends Component {
   render() {
+    console.log('viewItem in ViewSingleItem component ', this.props.viewItem);
     return (
       <article>
         <h1>This your single view</h1>
         <table>
           <tbody>
-          {this.props.item.map((single) => { return(
+          {this.props.viewItem.map((view) => {
+            return(
             <tr>
               <td className='imgSearchedWraper'>
-               <img width='100%' src={single.img_url} alt='one list item from all' />
+               <img width='100%' src={view.img_url} alt='one list item from all' />
               </td>
               <td className='showAllTitles'>
-                <h3><a href={single.exhibit_url} target='_blank'>{ single.title }</a></h3>
+                <h3><a href={view.exhibit_url} target='_blank'>{ view.title }</a></h3>
               </td>
               <td>
                 <textarea
                   placeholder='Enter your comments'
-                >{single.comment}</textarea>
+                >{view.comment}</textarea>
               </td>
-            </tr>
-            )})}
+            </tr>)})}
           </tbody>
         </table>
       </article>
