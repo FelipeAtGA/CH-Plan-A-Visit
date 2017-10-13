48e4b244d453c61267e20447885ce8652655b7ea
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-13 08:22:35 -0400

    ShowAlldb edit, transfer layout to ShowOne component and add export ShowOne into file for rendering

--- client/src/components/ShowAlldb.js
+++ client/src/components/ShowAlldb.js
@@ -1,36 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ShowOne from './ShowOne';
 
 class ShowAlldb extends Component {
   render() {
     console.log('ShowAlldb data ', this.props.dbItems)
     return(
-      <section>
-        <h1>List of your items</h1>
-        <table>
-          <tbody>
-          {this.props.dbItems.map((dbItem) => {
-              return(
-                <tr>
-                  <td className='imgSearchedWraper'>
-                   <img width='100%' src={dbItem.img_url} alt='one list item from all' />
-                  </td>
-                  <td className='showAllTitles'>
-                    <h3><a href={dbItem.exhibit_url} target='_blank'>{ dbItem.title }</a></h3>
-                  </td>
-                  <td>
-                    <button
-                      onClick={ (id) => {
-                        this.props.handleDeleteItem(dbItem.id);
-                      }}
-                    >Delete</button>
-                  </td>
-                </tr>
-              )}
+      <table>
+        <tbody>
+        {this.props.dbItems.map((dbItem) => {
+            return(
+              <ShowOne
+                dbItem={dbItem}
+                handleDeleteItem={ this.props.handleDeleteItem }
+                handleShowOne={ this.props.handleShowOne}
+              />
             )}
-          </tbody>
-        </table>
-      </section>
+          )}
+        </tbody>
+      </table>
     )
   }
 }
