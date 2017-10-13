e36c6e71067186ba0fc795cea288e5f4fd80d156
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-13 10:15:40 -0400

    Seed file edit add sample string to new column comment

--- db/seeds/item_seed_visitor_planner.sql
+++ db/seeds/item_seed_visitor_planner.sql
@@ -1,8 +1,9 @@
 \c visitor_palnner
 
-INSERT INTO items (title, img_url, exhibit_url)
+INSERT INTO items (title, img_url, exhibit_url, comment)
 VALUES
   ('Poster, London Music Festival, 1939',
   'https://images.collection.cooperhewitt.org/254119_de83d10266e169dd_b.jpg',
-  'https://collection.cooperhewitt.org/objects/18446845/'
+  'https://collection.cooperhewitt.org/objects/18446845/',
+  'This is dummy data to test table/rows/db'
   );
