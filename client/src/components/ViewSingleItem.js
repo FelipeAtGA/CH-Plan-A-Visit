17396bbb5179ef22810c8d5135bae1f402fa650e
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-13 08:12:11 -0400

    create and edit ViewSingleItem file

---
+++ client/src/components/ViewSingleItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class ViewSingleItem extends Component {
+  render() {
+    return (
+      <article>
+        <h1>This your single view</h1>
+        <table>
+          <tbody>
+          {this.props.item.map((single) => { return(
+            <tr>
+              <td className='imgSearchedWraper'>
+               <img width='100%' src={single.img_url} alt='one list item from all' />
+              </td>
+              <td className='showAllTitles'>
+                <h3><a href={single.exhibit_url} target='_blank'>{ single.title }</a></h3>
+              </td>
+              <td>
+                <textarea
+                  placeholder='Enter your comments'
+                ></textarea>
+              </td>
+            </tr>
+            )})}
+          </tbody>
+        </table>
+      </article>
+    )
+  }
+}
+
+export default ViewSingleItem;
