6885e7e819a83b18676e8374cb11b969fafbee85
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-14 15:46:36 -0400

    Footer change div tag to footer tag for html semantic layout

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return(
-      <div>
+      <footer>
         <div className="App-footer">
           <h6>Made with Love by Felipe Hernandez 2017 </h6>
         </div>
-      </div>
+      </footer>
     )
   };
 }
