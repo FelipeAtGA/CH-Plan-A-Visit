7e4d8d800f684b159eb6bb51c0b01715b855359e
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-15 15:07:36 -0400

    Home edit entire file

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render(){
     return(
-      <h1>Welcome Home</h1>
+      <article>
+       <h1>Welcome Home</h1>
+       <div className='homePageWrapper'>
+       <div id="column1" className='homePageDivs'>
+         <h2>Current</h2>
+         <h2>Exhibitions</h2>
+       </div>
+       <div id="column2" className='homePageDivs'>
+         <h2>Search the</h2>
+         <h2>Collection</h2>
+       </div>
+       <div id="column3" className='homePageDivs'>
+         <h2>Your Saved</h2>
+         <h2>Collection</h2>
+       </div>
+       <div id="column4" className='homePageDivs'>
+         <h2>About the</h2>
+         <h2>Museum</h2>
+       </div>
+       </div>
+      </article>
     )
   }
 }
