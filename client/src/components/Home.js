e7e4cda3df3c313133f943307203b51617b8ba2e
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-15 20:25:57 -0400

    Home delete h1 tag

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,23 +5,18 @@ class Home extends Component {
   render(){
     return(
       <article>
-       <h1>Welcome Home</h1>
        <div className='homePageWrapper'>
        <div id="column1" className='homePageDivs'>
-         <h2>Current</h2>
-         <h2>Exhibitions</h2>
+         <h2>Current Exhibitions</h2>
        </div>
        <div id="column2" className='homePageDivs'>
-         <h2>Search the</h2>
-         <h2>Collection</h2>
+         <h2>Search the Collection</h2>
        </div>
        <div id="column3" className='homePageDivs'>
-         <h2>Your Saved</h2>
-         <h2>Collection</h2>
+         <h2>Your Saved Collection</h2>
        </div>
        <div id="column4" className='homePageDivs'>
-         <h2>About the</h2>
-         <h2>Museum</h2>
+         <h2>About the Museum</h2>
        </div>
        </div>
       </article>
