0b56d6e064a2fb3a83833cb59e20aec92d804498
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-16 23:09:32 -0400

    Home change layout using justify-content

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,20 +4,28 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render(){
     return(
-      <article>
+      <article className='homePanelsWrapper'>
        <div className='homePageWrapper'>
-       <div id="column1" className='homePageDivs'>
-         <h2>Current Exhibitions</h2>
-       </div>
-       <div id="column2" className='homePageDivs'>
-         <h2>Search the Collection</h2>
-       </div>
-       <div id="column3" className='homePageDivs'>
-         <h2>Your Saved Collection</h2>
-       </div>
-       <div id="column4" className='homePageDivs'>
-         <h2>About the Museum</h2>
-       </div>
+       <Link to='/current'>
+           <div id="column1" className='homePageDivs'>
+             <h2>Current Exhibitions</h2>
+           </div>
+         </Link>
+         <Link to='/search'>
+           <div id="column2" className='homePageDivs'>
+             <h2>Search the Collection</h2>
+           </div>
+           </Link>
+         <Link to='/allItems'>
+           <div id="column3" className='homePageDivs'>
+             <h2>Your Saved Collection</h2>
+           </div>
+         </Link>
+         <Link to='/about'>
+           <div id="column4" className='homePageDivs'>
+             <h2>About the Museum</h2>
+           </div>
+         </Link>
        </div>
       </article>
     )
