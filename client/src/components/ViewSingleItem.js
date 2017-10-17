bd781a8558ceb13eed2ba4eb2794e5c2af1ccb2c
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-16 23:13:14 -0400

    ViewSingleItem add key to html tags

--- client/src/components/ViewSingleItem.js
+++ client/src/components/ViewSingleItem.js
@@ -4,17 +4,18 @@ class ViewSingleItem extends Component {
   render() {
     console.log('viewItem in ViewSingleItem component ', this.props.viewItem);
     return (
-      <article>
+      <article className='main'>
         <h1>This your single view</h1>
         <table className='tableSingleItem'>
           <tbody>
           {this.props.viewItem.map((view) => {
             return(
-              <tr>
+              <tr key={ view.id }>
                 <tr className='listSingle'>
                   <td className='imgSearchedWraperSingle'>
                     <h3><a href={view.exhibit_url} target='_blank'>{ view.title }</a></h3>
-                   <img width='100%' src={view.img_url} alt='one list item from all' />
+                    <img width='100%' src={view.img_url} alt='one list item from all' />
+                    <p className='comment'><i class="fa fa-quote-left" aria-hidden="true"></i>&nbsp;{ view.comment }&nbsp;<i class="fa fa-quote-right" aria-hidden="true"></i></p>
                   </td>
                 </tr>
                 <tr className='lowerRowSingleItem'>
@@ -29,7 +30,7 @@ class ViewSingleItem extends Component {
                         value={this.props.inputAddComment}
                         placeholder='Enter your comment'
                         onChange={this.props.handleInputCommentOnChange}
-                      >{ view.comment }</textarea>
+                      ></textarea>
                       <button id='submit'
                               className='buttonSingleItem'
                               type='button'
