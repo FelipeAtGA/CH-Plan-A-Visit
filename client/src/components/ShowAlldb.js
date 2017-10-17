2f18388505631695cc88ca8556863e2d6f7f7848
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-16 23:11:57 -0400

    ShowAlldb add key to html tags

--- client/src/components/ShowAlldb.js
+++ client/src/components/ShowAlldb.js
@@ -5,14 +5,15 @@ class ShowAlldb extends Component {
   render() {
     console.log('ShowAlldb data ', this.props.dbItems)
     return(
-      <article>
+      <article className='main'>
         <h1 className='title'>List of added artwork</h1>
         <table>
           <tbody>
-          {this.props.dbItems.map((dbItem) => {
+          {this.props.dbItems.map((dbItem,i) => {
               return(
                 <ShowOne
                   dbItem={dbItem}
+                  key={dbItem.id.toString()}
                   handleDeleteItem={ this.props.handleDeleteItem }
                   handleViewSingleItem={ this.props.handleViewSingleItem}
                 />
