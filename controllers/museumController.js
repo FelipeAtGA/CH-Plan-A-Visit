2b0bb7885de15768c5d9fcc239dae974e8a1976c
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-27 14:57:17 -0400

    museumController add a search method to handleSubmit method

--- controllers/museumController.js
+++ controllers/museumController.js
@@ -13,4 +13,30 @@ museumAPIcontroller.index = (req, res) => {
   });
 }
 
+museumAPIcontroller.search = (req, res) => {
+  console.log('in museum controller keyword ', req.query.keys)
+  let url = 'https://api.collection.cooperhewitt.org/rest/'
+    url += '?method=cooperhewitt.search.collection&access_token='
+    url += '49525b3629ac5bb3d446da768b0305a7&has_images=1&page=1&per_page=10'
+    url += '&query='
+    url +=  req.query.keys
+    url += '&description='
+    url +=  req.query.keys;
+    axios(url)
+    .then((response) => {
+      if(response.data.objects.length === 0){
+        window.alert('No such keywords exit in the database.\nPlease enter new keywords');
+        this.setState({
+          inputSearchValue: '',
+        })
+      }else{
+          res.json({
+            objects: response.data.objects,
+          })
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+}
+
 module.exports = museumAPIcontroller;
