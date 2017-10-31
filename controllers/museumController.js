bb3e059c94bbe399960462843d7619102a067106
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-30 21:38:56 -0400

    comment out console logs in museum controller

--- controllers/museumController.js
+++ controllers/museumController.js
@@ -14,9 +14,8 @@ museumAPIcontroller.index = (req, res) => {
 }
 
 museumAPIcontroller.search = (req, res) => {
-  console.log('in museum controller keyword ', req.query.keys)
+  //console.log('in museum controller keyword ', req.query.keys);
   let url = `https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.search.collection&access_token=${process.env.SECRET}&has_images=1&page=1&per_page=10&query=${req.query.keys}&description=${req.query.keys}`;
-  console.log('url in museum controller ', url)
     axios(url)
     .then((response) => {
       res.json({
